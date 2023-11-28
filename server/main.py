765628f563688e2939c2a348e8e99bcd1de1fe36
Mariano Aguirre Giraldo <nanomag.ag@gmail.com>
2023-11-28 18:38:24 -0500

    Add cors support and rename text to content

--- server/main.py
+++ server/main.py
@@ -1,19 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from services import WordCounter
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class RequestBody(BaseModel):
-    text: str
+    content: str
 
 
 @app.post("/words/count")
 async def count_words(body: RequestBody):
-    cleaned_text = body.text.split(" ")
+    cleaned_text = body.content.split(" ")
 
     word_counter = WordCounter()
     words = word_counter.count_words(cleaned_text)
