8c4eb28768e201f993cd275c6256fc9ee9badf53
Mariano Aguirre Giraldo <nanomag.ag@gmail.com>
2023-11-28 18:38:55 -0500

    Add UI for the form and an empty list

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,34 @@
+"use client";
+
+import { FormEvent } from "react";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="p-24">
+      <h1 className="text-xl text-center mb-10">Word counter</h1>
+
+      <section className="columns-2">
+        <form className="flex flex-col gap-5">
+          <textarea
+            name="content"
+            id="content"
+            cols={30}
+            rows={10}
+            placeholder="Enter your content here..."
+            className="border p-5"
+          ></textarea>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 py-2"
+          >
+            Default
+          </button>
+        </form>
+
+        <ul>
+          <li>Word: 1</li>
+        </ul>
+      </section>
+    </main>
   );
 }
