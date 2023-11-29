c805e1cb33ece559bebdcfb9456de1d252176e63
Mariano Aguirre Giraldo <nanomag.ag@gmail.com>
2023-11-28 19:36:41 -0500

    Add new endpoint to handle file input

--- server/main.py
+++ server/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import io
+
+from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
@@ -23,6 +25,19 @@ class RequestBody(BaseModel):
 async def count_text_words(body: RequestBody):
     word_counter = WordCounter()
     words = word_counter.count_words(body.content)
+    sorted_count = word_counter.sort_by_count(words)
+
+    return sorted_count
+
+
+@app.post("/files/count")
+async def count_file_words(file: UploadFile):
+    file_content = await file.read()
+    file_io = io.BytesIO(file_content)
+    content = file_io.getvalue().decode("utf-8")
+
+    word_counter = WordCounter()
+    words = word_counter.count_words(content)
     sorted_count = word_counter.sort_by_count(words)
 
     return sorted_count
