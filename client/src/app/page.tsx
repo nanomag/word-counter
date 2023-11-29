3309366587ee61fc80644c8cf60e28f7668f36e1
Mariano Aguirre Giraldo <nanomag.ag@gmail.com>
2023-11-28 19:00:03 -0500

    Save words count in state and render in list

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 
 export default function Home() {
+  const [wordsCount, setWordsCount] = useState<{} | null>(null);
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
@@ -15,15 +17,20 @@ export default function Home() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ content }),
-    });
+    })
+      .then((res) => res.json())
+      .then((data) => setWordsCount(data));
   }
 
   return (
-    <main className="p-24">
-      <h1 className="text-xl text-center mb-10">Word counter</h1>
+    <main className="p-10 h-full">
+      <h1 className="text-2xl font-bold text-center mb-10">Word counter</h1>
 
-      <section className="columns-2">
-        <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+      <section className="flex flex-col md:flex-row gap-10">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-5 w-full md:w-1/2"
+        >
           <textarea
             name="content"
             id="content"
@@ -32,17 +39,30 @@ export default function Home() {
             placeholder="Enter your content here..."
             className="border p-5"
           ></textarea>
+
           <button
             type="submit"
             className="text-white bg-blue-700 hover:bg-blue-800 py-2"
           >
-            Default
+            Count
           </button>
         </form>
 
-        <ul>
-          <li>Word: 1</li>
-        </ul>
+        {wordsCount ? (
+          <div className="flex flex-col md:w-1/2 h-screen overflow-y-scroll">
+            <h2 className="text-lg font-semibold mb-3">Words count</h2>
+
+            <ul className="max-w-md space-y-1 list-disc list-inside">
+              {Object.entries(wordsCount).map(([word, count]: any) => (
+                <li key={word}>
+                  {word}: {count}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>No words count yet.</p>
+        )}
       </section>
     </main>
   );
